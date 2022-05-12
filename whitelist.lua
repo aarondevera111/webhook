d5546d64af9e2aee45b97926dbcac4d854435f68
hasdhashdasdas <101713683+darkspicy@users.noreply.github.com>
2022-05-12 23:37:59 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,9 +1,9 @@
 userids = {
-[0] = true,
-[0] = true,
-[0] = true,
-[0] = true,
-[0] = true,
-[0] = true
+[1] = true,
+[2] = true,
+[3] = true,
+[4] = true,
+[5] = true,
+[6] = true
 }
 return userids
